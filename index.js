e948e5afe5bfb5a5d12fec4cbf1e6b66dcc19ec2
JohannaHaer <johanna.haerdter@gmail.com>
2024-02-26 13:34:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import fs from 'fs'
+
+const app = express()
+const PORT = 3010
+
+app.use(express.static('public'))
+
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+})
